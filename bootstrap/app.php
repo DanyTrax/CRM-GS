e84bde0197be11fea5adcd8f399e3aea25206849
DanyTrax <danny9203@gmail.com>
2026-01-16 23:29:30 -0500

    Registrar middlewares de Spatie Permission: role, permission y role_or_permission

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         // Registrar alias para middleware
         $middleware->alias([
             'client' => \App\Http\Middleware\CheckClientAccess::class,
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {

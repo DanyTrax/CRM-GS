42f0c5dc6b520cb68805fbc3d9616c82c59c7044
DanyTrax <danny9203@gmail.com>
2026-01-28 00:14:06 -0500

    Fix: Agregar grupo de navegación Mensajería

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -63,6 +63,7 @@ class AdminPanelProvider extends PanelProvider
                 'Gestión',
                 'Facturación',
                 'Soporte',
+                'Mensajería',
                 'Configuración',
             ]);
     }
